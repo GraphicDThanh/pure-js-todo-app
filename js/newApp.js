8be4b57e5a7ddbef9d966dc89a1f6d288702ec98
aleksUIX <aleksander@uixlimited.com>
2014-06-16 03:27:23 +0100

    pubsub cleaning

--- js/newApp.js
+++ js/newApp.js
@@ -3,8 +3,8 @@
 (function () {
     'use strict';
 
-    var doc = document,
-        ServiceLocalStorage = function (queryName) { // Services for local storage. Easily replaceable with custom service.
+    var doc = document, // reference global object
+        ServiceLocalStorage = function (queryName) { // Services plugin for local storage. Easily replaceable with custom service.
             var get = function () {
                     return JSON.parse(localStorage.getItem(queryName)) || [];
                 },
@@ -82,7 +82,8 @@
         },
 
 
-        TodoView = function (element) { // View renders and updates the UI
+        TodoView = function (element, observer) { // View renders and updates the UI
+            this.element = element;
             var $this = this,
 
                 sum = function (a, b) {
@@ -90,9 +91,10 @@
                 },
 
                 render = function (collection) {
-                    console.log(collection);
-                    $this.element.innerHTML = '';
-                    var listFrag = doc.createDocumentFragment(); // create and attach to a document partial before inserting into DOM
+                    var listEl = $this.element.getElementsByTagName('ul')[0],
+                        listFrag = doc.createDocumentFragment(); // create and attach to a document partial before inserting into DOM
+
+                    listEl.innerHTML = '';
                     listFrag.innerHTML = '';
 
                     for (var i = 0; i < collection.length; i++) {
@@ -107,41 +109,38 @@
                         listFrag.innerHTML += liFrag.reduce(sum);
                     }
 
-                    console.log('rendered');
-                    element.innerHTML = listFrag.innerHTML; // insert into DOM only once per render
-                }
+                    listEl.innerHTML = listFrag.innerHTML; // insert into DOM only once per render
+                },
 
-            this.element = element;
+                bindActions = function() {
+                    $this.element.onclick = function (e) { // delegate the events so we do not have to reattach on updates
+                        var el = e.target,
+                            wrapper = el.parentNode.parentNode,
+                            todoId = wrapper.dataset.todoid;
 
+                        if (el) {
+                            if (el.classList.contains('btn-delete')) {
+                                observer.publish('todoRemove', todoId);
+                            }
+                            if (el.type === 'checkbox') {
+                                observer.publish('toggleState', todoId);
+                            }
+                            if (el.id === 'todoAdd') {
+                                var content = doc.getElementById('todoAdd-content').value;
+                                observer.publish('todoAdd', new TodoModel(content, true));
+                            }
+                        }
+                    };
+                };
+
+            bindActions();
             return {
                 render: render
             };
         },
 
 
-        TodoEvents = function (element, observer) { // UI events are delegated to subelements of the wrapper for easy reattaching
-            element.onclick = function (e) {
-                var el = e.target,
-                    wrapper = el.parentNode.parentNode,
-                    todoId = wrapper.dataset.todoid.length > 0 ? wrapper.dataset.todoid : null;
-
-                if (el) {
-                    if (el.classList.contains('btn-delete')) {
-                        observer.publish('todoRemove', todoId);
-                    }
-                    if (el.type === 'checkbox') {
-                        observer.publish('toggleState', todoId);
-                    }
-                    if (el.id === 'todoAdd') {
-                        var content = doc.getElementById('todoAdd-content').value;
-                        observer.publish('todoAdd', new TodoModel(content, true));
-                    }
-                }
-            };
-        },
-
-
-        TodoObserver = function() {
+        TodoObserver = function() { // This is a sandbox for the application, which takes form of a simple publish / subscribe pattern
             this.topics = [];
             var $this = this,
                 subscribe = function(topic, module, func, args) {
@@ -153,8 +152,14 @@
                 },
 
                 unsub = function(topic, module) {
-                    console.log($this.topics[topic].indexOf(module));
-                    //$this.topics[topic].splice($this.topics[topic].indexOf(module), 1);
+                    var subscribers = $this.topics[topic] || [];
+                    if (subscribers.length > 0) {
+                        for (var i = 0; i < subscribers.length; i++) {
+                            if (subscribers[i].module === module) {
+                                subscribers.splice(i, 1);
+                            }
+                        }
+                    }
                 },
 
                 publish = function(topic, args) {
@@ -178,14 +183,14 @@
         };
 
 
-
+    // Initialise all modules
     var todoObserver = new TodoObserver(),
-        todoView = new TodoView(doc.getElementById('todoList')),
+        todoView = new TodoView(doc.getElementById('listWrapper'), todoObserver),
         todoCollection = new TodoCollection(todoObserver),
-        serviceLocalStorage = new ServiceLocalStorage('todos'),
-        todoEvents = new TodoEvents(doc.getElementById('listWrapper'), todoObserver);
+        serviceLocalStorage = new ServiceLocalStorage('todos');
 
 
+    // Register all subscriptions
     todoObserver.subscribe('init', todoCollection, todoCollection.init, serviceLocalStorage.get);
     todoObserver.subscribe('dataChange', serviceLocalStorage, serviceLocalStorage.set, todoCollection.getItems);
     todoObserver.subscribe('dataChange', todoView, todoView.render, todoCollection.getItems);
@@ -195,8 +200,8 @@
     todoObserver.subscribe('todoRemove', todoCollection, todoCollection.deleteItem);
     todoObserver.subscribe('todoAdd', todoCollection, todoCollection.save);
 
-    todoObserver.unsub('dataChange', serviceLocalStorage);
 
+    //Kick off the app
     todoObserver.publish('init');
 
 })();
